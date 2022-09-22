35b4eba71e678b10c701ddfa571edbb81852195d
Opilane TTHK <opilane@tthk.ee>
2022-09-22 15:51:02 +0300

    University models

---
+++ University/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿
+using System.Collections.Generic;
+
+namespace University.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string Title { get; set; }
+        public int Credits { get; set; }
+
+
+        public ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
