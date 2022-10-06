decc300098364dbec1ab438846598b97de6d2ec6
Opilane TTHK <opilane@tthk.ee>
2022-10-06 13:02:28 +0300

    change

--- University/Models/Student.cs
+++ University/Models/Student.cs
@@ -7,6 +7,12 @@ namespace University.Models
 {
     public class Student
     {
-        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        public DateTime EnrollmentDate { get; set; }
+        public ICollection<Enrollment> Enrollments { get; set; }
+
+
     }
 }
