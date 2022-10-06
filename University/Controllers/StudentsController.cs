971fdb6afdeca579a4f6e7035655f056282f708c
Opilane TTHK <opilane@tthk.ee>
2022-10-06 15:52:20 +0300

    06-10

---
+++ University/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+
+namespace University.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var result = await _context.Students.ToListAsync();
+
+            return View(result);
+
+        }
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var student = await _context.Students
+                .Include(s => s.Enrollments)
+                    .ThenInclude(e => e.Course)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(m => m.StudentId == id);
+
+            if (student == null)
+            {
+                return NotFound();
+            }
+
+            return View(student);
+        }
+    }
+}
